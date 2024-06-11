97e5f512377cdd974a4ac980c56a5d320c2e3518
Harshit-Bhakta <hz1927@srmist.edu.in>
2024-06-11 20:34:56 +0530

    updated

--- lib/pages/gift_card_page.dart
+++ lib/pages/gift_card_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class GiftCardPage extends StatelessWidget {
   const GiftCardPage({super.key, required Null Function() onTap});
@@ -59,38 +61,319 @@ class GiftCardPage extends StatelessWidget {
                       fontSize: 23,
                       fontWeight: FontWeight.bold),
                 ),
-                const SizedBox(width: 110,),
-                Text("FAQs", style: TextStyle(color: Colors.greenAccent, fontSize: 17, fontWeight: FontWeight.w500),)
+                const SizedBox(
+                  width: 110,
+                ),
+                Text(
+                  "FAQs",
+                  style: TextStyle(
+                      color: Colors.greenAccent,
+                      fontSize: 17,
+                      fontWeight: FontWeight.w500),
+                )
               ],
             ),
           ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(top: 20,left: 15, right: 15),
-        child: Container(
-          height: 100,
-          width: 400,
-          decoration: BoxDecoration(
-             borderRadius: BorderRadius.circular(20),
-                  gradient: LinearGradient(colors: [
-                    Colors.deepPurple.shade400,
-                    Colors.deepPurple.shade700
-                  ], begin: Alignment.centerRight, end: Alignment.centerLeft)
-          ),
-          child: Container(
-            height: 40,
-            width: 100,
+      body:  Padding(
+  padding: const EdgeInsets.only(top: 20, left: 15, right: 15),
+  child: Column(
+    children: [
+      Stack(
+        children: [
+          Container(
+            height: 100,
+            width: 400,
             decoration: BoxDecoration(
-              gradient: LinearGradient(colors: [Colors.black,Colors.black],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomLeft,
+              borderRadius: BorderRadius.circular(20),
+              gradient: LinearGradient(
+                colors: [
+                  Colors.deepPurple.shade400,
+                  Colors.deepPurple.shade700,
+                ],
+                begin: Alignment.centerRight,
+                end: Alignment.centerLeft,
               ),
-              borderRadius: BorderRadius.circular(15)
             ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20, top: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Rs 0.00',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      'available balance',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 20),
+                  child: Container(
+                    height: 50,
+                    width: 150,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                          BoxShadow(
+                              color: Colors.black.withOpacity(0.5),
+                              offset: Offset(0, 4),
+                              blurRadius: 15,
+                              spreadRadius: 1)
+                        ],
+                      gradient: LinearGradient(
+                        colors: [
+                          Color.fromARGB(255, 0, 0, 0),
+                          Color.fromARGB(255, 0, 0, 0),
+                        ],
+                        begin: Alignment.centerRight,
+                        end: Alignment.centerLeft,
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Claim gift card',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 17,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      const SizedBox(height: 130),
+      Padding(
+        padding: const EdgeInsets.only(left: 80),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    height: 70,
+                    width: 235,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.white,
+                      border: Border.all(
+                        width: 5,
+                        color: const Color.fromARGB(255, 0, 0, 0).withOpacity(0.8),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: const Color.fromARGB(255, 255, 255, 255).withOpacity(0.3),
+                          offset: Offset(-8, 8),
+                          blurRadius: 5,
+                          spreadRadius: 0.2,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 20, top: 20),
+                        child: Container(
+                          height: 30,
+                          width: 30,
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 255, 0, 128),
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 25),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20),
+                        child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              height: 10,
+                              width: 100,
+                              decoration: BoxDecoration(
+                                color: Colors.grey.shade400,
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            const SizedBox(height: 10,),
+                            Container(
+                              height: 10,
+                              width: 70,
+                              decoration: BoxDecoration(
+                                color: Colors.grey.shade400,
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20), 
+              Stack(
+                children: [
+                  Container(
+                    height: 70,
+                    width: 235,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.white,
+                      border: Border.all(
+                        width: 5,
+                        color: const Color.fromARGB(255, 0, 0, 0).withOpacity(0.8),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: const Color.fromARGB(255, 255, 255, 255).withOpacity(0.3),
+                          offset: Offset(-8, 8),
+                          blurRadius: 5,
+                          spreadRadius: 0.2,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 20, top: 20),
+                        child: Container(
+                          height: 30,
+                          width: 30,
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 255, 132, 0),
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 25),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20),
+                        child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              height: 10,
+                              width: 100,
+                              decoration: BoxDecoration(
+                                color: Colors.grey.shade400,
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            Container(
+                              height: 10,
+                              width: 70,
+                              decoration: BoxDecoration(
+                                color: Colors.grey.shade400,
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+               const SizedBox(height: 20), 
+              Stack(
+                children: [
+                  Container(
+                    height: 70,
+                    width: 235,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.white,
+                      border: Border.all(
+                        width: 5,
+                        color: const Color.fromARGB(255, 0, 0, 0).withOpacity(0.8),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: const Color.fromARGB(255, 255, 255, 255).withOpacity(0.3),
+                          offset: Offset(-8, 8),
+                          blurRadius: 5,
+                          spreadRadius: 0.2,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 20, top: 20),
+                        child: Container(
+                          height: 30,
+                          width: 30,
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 230, 246, 5),
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 25),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20),
+                        child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              height: 10,
+                              width: 100,
+                              decoration: BoxDecoration(
+                                color: Colors.grey.shade400,
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            Container(
+                              height: 10,
+                              width: 70,
+                              decoration: BoxDecoration(
+                                color: Colors.grey.shade400,
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
+      const SizedBox(height: 50,),
+      Text('New transactions will appear here as you start\n                  using Gift Card Balance', style: TextStyle(color: Colors.white, fontSize: 15, fontWeight: FontWeight.w400),)
+    ],
+      ),
+    )
     );
   }
 }
